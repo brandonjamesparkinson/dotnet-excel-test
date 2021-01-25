e0b25e0fad78e7b178c3ac55fd0d91fc68101b16
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2021-01-25 15:52:08 +0000

    Write to excel complete

--- ExcelDemo/Program.cs
+++ ExcelDemo/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ExcelDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var file = new FileInfo(@"C:\Demos\YouTubeDemo.xlsx");
+
+            var people = GetSetupData();
+
+            await SaveExcelFile(people, file);
+        }
+
+        private static async Task SaveExcelFile(List<PersonModel> people, FileInfo file)
+        {
+            DeleteIfExists(file);
+
+            using var package = new ExcelPackage(file);
+
+            var ws = package.Workbook.Worksheets.Add("MainReport");
+
+            var range = ws.Cells["A1"].LoadFromCollection(people, true);
+            range.AutoFitColumns();
+
+            await package.SaveAsync();
+        }
+
+        private static void DeleteIfExists(FileInfo file)
+        {
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+        }
+
+        private static List<PersonModel> GetSetupData()
+        {
+            List<PersonModel> output = new()
+            {
+                new() { Id = 1, FirstName = "Brandon", LastName = "Parkinson" },
+                new() { Id = 2, FirstName = "Jane", LastName = "Smith" },
+                new() { Id = 3, FirstName = "Susan", LastName = "Storm" },
+            };
+
+            return output;
         }
     }
 }

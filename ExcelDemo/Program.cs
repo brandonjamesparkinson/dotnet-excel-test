b1569018318590f0f5e304455046694c08871483
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2021-01-25 15:58:32 +0000

    Formatting

--- ExcelDemo/Program.cs
+++ ExcelDemo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -26,8 +28,19 @@ namespace ExcelDemo
 
             var ws = package.Workbook.Worksheets.Add("MainReport");
 
-            var range = ws.Cells["A1"].LoadFromCollection(people, true);
+            var range = ws.Cells["A2"].LoadFromCollection(people, true);
             range.AutoFitColumns();
+
+            // Formats the header row 
+            ws.Cells["A1"].Value = "Test Report";
+            ws.Cells["A1:C1"].Merge = true;
+            ws.Column(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            ws.Row(1).Style.Font.Size = 24;
+            ws.Row(1).Style.Font.Color.SetColor(Color.Blue);
+
+            ws.Row(2).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            ws.Row(2).Style.Font.Bold = true;
+            ws.Column(3).Width = 20;
 
             await package.SaveAsync();
         }
